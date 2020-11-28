bec8f2f9bd10dcd55ee2269daeee0ff1118ff084
James Kulu <jameskulu55@gmail.com>
2020-11-28 10:05:13 +0545

    (add) Adds question number 15 of data types

--- Data Types/15.py
+++ Data Types/15.py
@@ -2,3 +2,12 @@
 # Sample function and result :
 # insert_sting_middle('[[]]<<>>', 'Python') -> [[Python]]
 # insert_sting_middle('{{}}', 'PHP') -> {{PHP}}
+
+
+def insert_string(outer_string, inner_string):
+    outer_first = outer_string[:2]
+    outer_last = outer_string[2:4]
+    return f'{outer_first}{inner_string}{outer_last}'
+
+
+print(insert_string('[[]]<<>>', 'Python'))
